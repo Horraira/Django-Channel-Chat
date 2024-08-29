c986eab62882294cf7b9b23e70b6d5528076bb51
Horraira <abuhorraira1432@gmail.com>
2024-08-29 17:44:14 +0600

    handle user joined

--- static/js/streams.js
+++ static/js/streams.js
@@ -8,15 +8,17 @@ const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
 // index 0 is the microphone and index 1 is the camera
 let localTracks = []
 let remoteUsers = {}
+let member = {
+    name: "John Doe"  // Example name
+};
 
 let joiAndDisplayLocalStream = async () => {
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
-
-    let member = {
-        name: "John Doe"  // Example name
-    };
 
     let player = `<div  class="video-container" id="user-container-${UID}">
                      <div class="video-player" id="user-${UID}"></div>
@@ -28,6 +30,35 @@ let joiAndDisplayLocalStream = async () => {
     localTracks[1].play(`user-${UID}`)
 
     await client.publish([localTracks[0], localTracks[1]])
+}
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if (mediaType === 'video') {
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null) {
+            player.remove()
+        }
+
+        player = `<div  class="video-container" id="user-container-${user.uid}">
+                     <div class="video-player" id="user-${user.uid}"></div>
+                     <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
+                  </div>`
+
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+
+    if (mediaType === 'audio') {
+        user.audioTrack.play()
+    }
+}
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
 }
 
 joiAndDisplayLocalStream()
